de9b1beecfa65f5daeb5f93a31d32c6b474de1ab
ArsenGoguev <agoguev33@gmail.com>
2024-08-15 17:59:12 +0300

    исправление бага с постоянными get-запросами

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { Online, Offline } from 'react-detect-offline'
-import { Alert } from 'antd'
 
 import store from './store/store.js'
 import './index.scss'
@@ -12,21 +10,8 @@ import App from './App/App.jsx'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Online>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Online>
-    <Offline>
-      <Alert
-        showIcon
-        banner
-        type="error"
-        message="There is no internet connection"
-        description="It looks like you have problems with the network. Check your internet connection."
-      />
-    </Offline>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
-
-// постоянные get-запросы должны отключаться в production
